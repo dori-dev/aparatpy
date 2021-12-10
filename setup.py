1468c3d01d262747221343995bc2e70f8023a27f
mohammad <mr.dori.dev@gmail.com>
2021-12-10 10:23:16 +0330

    update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="aparatpy",
-    version="1.0.0",
+    version="1.1.0",
     author="Mohammad Dori",
     author_email="mr.dori.dev@gmail.com",
     description="aparat.com downloader",
