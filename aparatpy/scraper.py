569617d113534c26127c34fda96da090cc9dbd84
mohammad <mr.dori.dev@gmail.com>
2021-12-10 10:30:05 +0330

    add functions docstring

--- aparatpy/scraper.py
+++ aparatpy/scraper.py
@@ -1,4 +1,4 @@
-"""scraper module
+"""scraper module for aparatpy project
 """
 from typing import List
 import re
@@ -8,18 +8,26 @@ from exceptions import QualityError
 
 
 class Scraper:
+    """scrap the video page
+    """
+
     def __init__(self, url, quality):
         self.url = url
         self.quality = quality
 
-    def get_all_links(self):
+    def get_all_links(self) -> List[str]:
+        """get all links for download video
+
+        Returns:
+            list[str]: list of video links
+        """
         result = requests.get(self.url)
         content = BeautifulSoup(result.text, 'html.parser')
         video_links = content.find_all('a', href=re.compile("https://.*.mp4"))
         links = [link['href'] for link in video_links]
         return links
 
-    def get_link(self):
+    def get_link(self) -> str:
         links = self.get_all_links()
         available_qualities = self.get_qualities(links)
         if self.quality not in available_qualities:
@@ -30,6 +38,14 @@ class Scraper:
 
     @staticmethod
     def get_qualities(links: List[str]) -> List[str]:
+        """get all available qualities for video
+
+        Args:
+            links (List[str]): list of video links
+
+        Returns:
+            List[str]: list of available_qualities
+        """
         quality_re = re.compile(".*(-[0-9]{3,4}p).mp4.*")
         available_qualities = []
         for link in links:
